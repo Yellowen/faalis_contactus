721d6b5224492e168a6cc17ee3d8eeb1a0354700
sadmad <sarmad.re@gmail.com>
2016-06-11 14:27:15 +0430

    second step to make contact us module

--- app/controllers/faalis/dashboard/blog/categories_controller.rb
+++
@@ -1,8 +0,0 @@
-class Faalis::Dashboard::Blog::CategoriesController < ::Dashboard::ApplicationController
-  engine 'Faalis::Engine'
-  namespace 'blog'
-
-  in_form do |list|
-    list.attributes except: [:site, :user]
-  end
-end
--- test/fabricators/faalis/blog/category.rb
+++
@@ -1,9 +0,0 @@
-require 'faker'
-
-Fabricator :category, class_name: 'Faalis::Blog::Category' do
-  title { Faker::Name.title }
-
-  description 'sme desc'
-  permalink 'category-1'
-  members_only  false
-end
